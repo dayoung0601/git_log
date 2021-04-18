1f56d9e087c0f98942fea755c0d9c96aea885c63
dayoung0601 <dayoung0601@hanmail.net>
2021-04-18 12:41:41 +0900

    게시글 수정 작업중

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -41,6 +41,7 @@ const GridBox = styled.div`
     ${(props) => (props.bg? `background-color: ${props.bg};`: "")}
     ${(props) => (props.is_flex? `display: flex; align-items: center; justify-content: space-between;`:"")}
     ${(props) => (props.flex? `display: flex; align-items: center;`:"")}
+
 `;
 
 export default Grid; 
--- src/shared/App.js
+++ src/shared/App.js
@@ -3,26 +3,20 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
-<<<<<<< HEAD
 import { history } from "../redux/configureStore";
 
-import { useSelector } from "react-redux";
-
-=======
->>>>>>> 92b3f7fd7c4e5cd2759be2d4b83572b6ee2a9f2e
 import Header from "../components/Header";
 import Signup from '../pages/Signup';
 import Login from '../pages/Login';
 import PostList from '../pages/PostList';
+import PostWrite from '../pages/PostWrite';
 
-import { history } from "../redux/configureStore";
-import { useDispatch } from 'react-redux';
-import { actionCreators} from '../redux/modules/user';
+import { useSelector, useDispatch } from 'react-redux';
+import { actionCreators } from '../redux/modules/user';
 
 
 function App() {
   
-  const is_login = useSelector((state) => state.user.is_login);
   const dispatch = useDispatch();
   const local_token = localStorage.getItem("token") ? true : false;
 
@@ -45,10 +39,9 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/" exact component={PostList} />
             <Route path="/write" exact component={PostWrite} />
+            <Route path="/write/:id" exact component={PostWrite} />
           </ConnectedRouter>
-         { is_login && <WriteBtn
-            onClick={() => history.push("/write")}
-          >+ 새 글 등록</WriteBtn> }
+        
         </Responsive>
       </Background>
     </React.Fragment>
@@ -65,10 +58,13 @@ const Background = styled.div`
   height: 100%;
   background-color: ${(props) => props.theme.main_bg_color};
 `;
+
 const Responsive = styled.div`
   ${(prop) => prop.theme.responsiveContainer};
-  width: 70vw;
-  margin:auto;
+  width: 60vw;
+  margin: auto;
 `;
+
+
 
 export default App;
